ba5a5fb4ce6adffa1a1b6c7722be63990860e327
Javier <106848262+javiiicz@users.noreply.github.com>
2024-09-28 21:11:50 -0400

    CORS

--- link.py
+++ link.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 import subprocess
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/execute', methods=['POST'])
 def process_frame():
