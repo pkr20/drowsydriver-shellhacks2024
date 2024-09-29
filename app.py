c359bcde1eab193f27f70b97ba25aaff1cf1cc09
Javier <106848262+javiiicz@users.noreply.github.com>
2024-09-29 09:19:55 -0400

    rename to app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route('/execute', methods=['POST'])
 def process_frame():
-    process = subprocess.Popen(['python', 'eyechecker.py'])  # or just ['app.exe']
+    process = subprocess.Popen(['python3', 'eyechecker.py'])  # or just ['app.exe']
 
     # Your main script can continue running without waiting for the GUI to close
     print("The GUI application has been launched.")
